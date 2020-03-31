ad222963d176d11ac1bc70380cb840b2b4a64969
Michael Fladischer <michael@fladi.at>
2020-03-31 14:03:24 +0200

    Synchronize students to redis mapping cache.

--- src/outpost/django/campusonline/tasks.py
+++ src/outpost/django/campusonline/tasks.py
@@ -5,7 +5,7 @@ from celery.task import PeriodicTask
 from django.core.cache import caches
 from django.core.cache.backends.base import InvalidCacheBackendError
 
-from .models import Person
+from .models import Person, Student
 
 logger = logging.getLogger(__name__)
 
@@ -14,16 +14,28 @@ class UsernameSyncTask(PeriodicTask):
     run_every = timedelta(minutes=5)
 
     def run(self, **kwargs):
+        logger.info("Updating username redis cache")
         try:
             cache = caches["meduniverse"]
         except InvalidCacheBackendError:
             logger.info("No meduniverse cache defined, not running UsernameSyncTask")
             return
+        id_to_username = {}
+        username_to_id = {}
+
         persons = Person.objects.all()
-        id_to_username = {p.pk: p.username for p in persons}
-        cache.set("id_to_username", id_to_username)
-        username_to_id = {p.username: p.pk for p in persons}
-        cache.set("username_to_id", username_to_id)
+        id_to_username.update({p.pk: p.username for p in persons})
+        username_to_id.update({p.username: p.pk for p in persons})
         for p in persons:
             cache.set(f"username:{p.username}", p.pk)
             cache.set(f"id:{p.pk}", p.username)
+
+        students = Student.objects.all()
+        id_to_username.update({s.pk: s.username for s in students})
+        username_to_id.update({s.username: s.pk for s in students})
+        for s in students:
+            cache.set(f"username:{s.username}", s.pk)
+            cache.set(f"id:{s.pk}", s.username)
+
+        cache.set("id_to_username", id_to_username)
+        cache.set("username_to_id", username_to_id)
