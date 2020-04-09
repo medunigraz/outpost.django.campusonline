119aa3f0005c9283453977accb6184f236e80921
Michael Fladischer <michael@fladi.at>
2020-04-09 20:59:39 +0200

    Remove username-id mapping dicts, they are not used.

--- src/outpost/django/campusonline/tasks.py
+++ src/outpost/django/campusonline/tasks.py
@@ -20,22 +20,14 @@ class UsernameSyncTask(PeriodicTask):
         except InvalidCacheBackendError:
             logger.info("No meduniverse cache defined, not running UsernameSyncTask")
             return
-        id_to_username = {}
-        username_to_id = {}
 
         persons = Person.objects.all()
-        id_to_username.update({p.pk: p.username for p in persons})
-        username_to_id.update({p.username: p.pk for p in persons})
         for p in persons:
             cache.set(f"username:{p.username}", p.pk)
             cache.set(f"id:{p.pk}", p.username)
 
         students = Student.objects.all()
-        id_to_username.update({s.pk: s.username for s in students})
-        username_to_id.update({s.username: s.pk for s in students})
         for s in students:
             cache.set(f"username:{s.username}", s.pk)
             cache.set(f"id:{s.pk}", s.username)
 
-        cache.set("id_to_username", id_to_username)
-        cache.set("username_to_id", username_to_id)
