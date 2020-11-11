8575e5e05af092f14d0546f9500090d6991534bf
Michael Fladischer <michael@fladi.at>
2020-11-11 10:43:09 +0100

    Add search indizes for Room, Person and Student.

--- src/outpost/django/campusonline/search_indexes.py
+++ src/outpost/django/campusonline/search_indexes.py
@@ -1,7 +1,7 @@
 from celery_haystack.indexes import CelerySearchIndex
 from haystack import indexes
 
-from .models import BulletinPage
+from . import models
 
 
 class BulletinPageIndex(CelerySearchIndex, indexes.Indexable):
@@ -10,7 +10,46 @@ class BulletinPageIndex(CelerySearchIndex, indexes.Indexable):
     academic_year = indexes.CharField(model_attr="bulletin__academic_year")
 
     def get_model(self):
-        return BulletinPage
+        return models.BulletinPage
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+
+class PersonIndex(CelerySearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    presentation = indexes.CharField(use_template=True)
+    autocomplete = indexes.EdgeNgramField(use_template=True)
+
+    def get_model(self):
+        return models.Person
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+
+class StudentIndex(CelerySearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    presentation = indexes.CharField(use_template=True)
+    autocomplete = indexes.EdgeNgramField(use_template=True)
+
+    def get_model(self):
+        return models.Student
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+
+class RoomIndex(CelerySearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    presentation = indexes.CharField(use_template=True)
+    autocomplete = indexes.EdgeNgramField(use_template=True)
+
+    def get_model(self):
+        return models.Room
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
