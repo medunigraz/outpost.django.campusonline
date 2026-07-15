7291fd2f65ae21c8f28c197a22a31ac064595b4d
Michael Fladischer <michael.fladischer@medunigraz.at>
2026-07-15 13:06:33 +0200

    Add OrganizationAdmin and StudentAdmin.

--- src/outpost/django/campusonline/admin.py
+++ src/outpost/django/campusonline/admin.py
@@ -5,6 +5,14 @@ from outpost.django.base.admin import ReadOnlyAdminMixin
 from . import models
 
 
+@admin.register(models.Organization)
+class OrganizationAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ("__str__", "short", "parent", "type", "address")
+    list_display_links = ("__str__", "short")
+    list_filter = ("category", "type", "university_law")
+    search_fields = ("name", "short")
+
+
 @admin.register(models.Person)
 class PersonAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     list_display = ("last_name", "first_name", "title", "email", "username", "room")
@@ -12,6 +20,12 @@ class PersonAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     list_filter = ("sex", "employed")
     search_fields = ("first_name", "last_name", "email")
     exclude = ("avatar_private", "hash")
+
+
+@admin.register(models.Student)
+class StudentAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "email", "username", "cardid")
+    list_display_links = ("first_name", "last_name")
 
 
 class PersonInline(admin.TabularInline):
