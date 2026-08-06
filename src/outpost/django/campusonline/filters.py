933fe63b1bfc2668fb6cbf2b9deda58f8de8cfde
gwirn <71886945+gwirn@users.noreply.github.com>
2026-08-06 16:08:11 +0200

    fix name for filter

--- src/outpost/django/campusonline/filters.py
+++ src/outpost/django/campusonline/filters.py
@@ -62,7 +62,7 @@ class FunctionFilter(filterset.FilterSet):
                 "iregex",
             ),
             "leader": ("exact",),
-            "deputy_leader": ("exact",),
+            "deputy__leader": ("exact",),
         }
 
 
